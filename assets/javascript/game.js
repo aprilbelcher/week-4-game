692c72df30bb0505fb62d08e9143dfccc628ebb1
April Denise Belcher <aprildenise@Aprils-MacBook-Pro.local>
2017-10-30 08:57:39 -0700

    added class and id

---
+++ assets/javascript/game.js
@@ -0,0 +1,48 @@
+//GLOBAL VARIABLES
+//================================================
+//arrays and functions for hoding the data
+
+var crystalOptions = [ "green", "black", "pink", "blue"];
+var selectedCrystals = "";
+var pointsInCrystals = [];
+
+//Game Counters
+var crystalEarned
+var
+
+
+//FUNCTIONS
+//================================================
+//Reusable blocks of code that we can use when needed
+
+function startGame () {
+
+
+
+//// CRITICAL LINE *reset*
+
+
+
+//Testing and Debugging
+
+
+
+
+//change HTML to reflect round conditions
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+//MAIN PROCESS
+//================================================
